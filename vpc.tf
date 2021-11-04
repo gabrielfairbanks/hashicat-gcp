8baf5e24b828921db095c866025850ee99a4f320
Gabriel Fairbanks <gabriel.fairbanks@gmail.com>
2021-11-04 12:00:15 +0000

    trying to fix issue with network

--- vpc.tf
+++ vpc.tf
@@ -2,13 +2,13 @@ module "vpc" {
     source  = "terraform-google-modules/network/google"
     version = "~> 3.0"
 
-    project_id   = "${var.project}"
+    project_id   = var.project
     network_name = "fairbanks-network"
     routing_mode = "GLOBAL"
 
     subnets = [
   {
-    subnet_name   = "gaurav-subnet"
+    subnet_name   = "fairbanks-subnet"
     subnet_ip     = "10.100.10.0/24"
     subnet_region = var.region
   }
